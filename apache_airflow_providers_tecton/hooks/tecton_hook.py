51827fbb553d7b88d695ef631e6d365c15d3c13f
Alex Guziel <alex@tecton.ai>
2022-08-03 22:50:22 -0700

    changes + precommit

--- apache_airflow_providers_tecton/hooks/tecton_hook.py
+++ apache_airflow_providers_tecton/hooks/tecton_hook.py
@@ -17,7 +17,10 @@ import logging
 import pprint
 import sys
 import urllib
-from typing import Any, Dict, Union, Optional
+from typing import Any
+from typing import Dict
+from typing import Optional
+from typing import Union
 
 import pytz
 import requests
@@ -45,13 +48,11 @@ class TectonHook(BaseHook):
         Put your Tecton API key in the password field
         The default connection is `tecton_default`
     """
+
     conn_name_attr = "tecton_conn_id"
     default_conn_name = "tecton_default"
 
-    def __init__(
-            self,
-            conn_id: str = default_conn_name
-    ):
+    def __init__(self, conn_id: str = default_conn_name):
         super().__init__()
         self.tecton_conn_id = conn_id
         self._session = None
@@ -82,47 +83,87 @@ class TectonHook(BaseHook):
         else:
             return pprint.pformat(d)
 
-    def _make_request(self, conn: requests.Session, url: str, data: Dict[str, Any], verbose: bool = False) -> Dict[
-        str, Any]:
+    def _make_request(
+        self,
+        conn: requests.Session,
+        url: str,
+        data: Dict[str, Any],
+        verbose: bool = False,
+    ) -> Dict[str, Any]:
         full_path = urllib.parse.urljoin(self.base_url, url)
         if verbose:
-            logging.info(f"Making POST request to {full_path} with body=\n{self._pformat_dict(data)}")
+            logging.info(
+                f"Making POST request to {full_path} with body=\n{self._pformat_dict(data)}"
+            )
         resp = conn.post(full_path, json.dumps(data))
         try:
             resp.raise_for_status()
         except Exception as e:
             exc = e
-            if 'error' in resp.json():
+            if "error" in resp.json():
                 raise Exception(f"Tecton error: {resp.json()['error']}")
             raise e
         else:
             exc = None
         finally:
             if verbose or exc:
-                logging.info(f"Response: Code={resp.status_code} Body=\n{self._pformat_dict(resp.json())}")
+                logging.info(
+                    f"Response: Code={resp.status_code} Body=\n{self._pformat_dict(resp.json())}"
+                )
         return resp.json()
 
-    def _canonicalize_datetime(self, dt):
+    def _canonicalize_datetime(self, dt) -> str:
         if isinstance(dt, str):
-            return datetime.datetime.fromisoformat(dt).strftime('%Y-%m-%dT%H:%M:%SZ')
+            return datetime.datetime.fromisoformat(dt).strftime("%Y-%m-%dT%H:%M:%SZ")
         elif isinstance(dt, datetime.datetime):
-            return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
+            return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
         else:
             raise Exception("unexpected type for datetime: " + str(type(dt)))
 
     def _parse_time(self, dt):
-        return datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.UTC)
+        return datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%SZ").replace(
+            tzinfo=pytz.UTC
+        )
+
+    def find_job(
+        self,
+        workspace: str,
+        feature_view: str,
+        start_time: Union[datetime.datetime, str],
+        end_time: Union[datetime.datetime, str],
+        online: bool,
+        offline: bool,
+    ):
+        jobs = self.list_materialization_jobs(
+            workspace=workspace, feature_view=feature_view
+        ).get("jobs", [])
+        matching_job = None
+        for job in sorted(
+            jobs, reverse=True, key=lambda x: self._parse_time(x["created_at"])
+        ):
+            if job["job_type"].lower() != "batch":
+                continue
+            if (
+                job["online"] == online
+                and job["offline"] == offline
+                and job["start_time"] == self._canonicalize_datetime(start_time)
+                and job["end_time"] == self._canonicalize_datetime(end_time)
+            ):
+                matching_job = job
+                break
+        return matching_job
 
     def submit_materialization_job(
-            self,
-            workspace: str,
-            feature_view: str,
-            start_time: Union[datetime.datetime, str],
-            end_time: Union[datetime.datetime, str],
-            online: bool,
-            offline: bool,
-            allow_overwrite: bool = False,
-            tecton_managed_retries: bool = True) -> Dict:
+        self,
+        workspace: str,
+        feature_view: str,
+        start_time: Union[datetime.datetime, str],
+        end_time: Union[datetime.datetime, str],
+        online: bool,
+        offline: bool,
+        allow_overwrite: bool = False,
+        tecton_managed_retries: bool = True,
+    ) -> Dict:
         """
         Calls the /jobs/submit-materialization-job API. This submits a Tecton materialization job for a FeatureView.
 
@@ -137,22 +178,20 @@ class TectonHook(BaseHook):
         :return: The response from the Tecton API. Throws an exception if request was not successful.
         """
         data = {
-            'workspace': workspace,
-            'feature_view': feature_view,
-            'start_time': self._canonicalize_datetime(start_time),
-            'end_time': self._canonicalize_datetime(end_time),
-            'online': online,
-            'offline': offline,
-            'overwrite': allow_overwrite,
-            'use_tecton_managed_retries': tecton_managed_retries,
+            "workspace": workspace,
+            "feature_view": feature_view,
+            "start_time": self._canonicalize_datetime(start_time),
+            "end_time": self._canonicalize_datetime(end_time),
+            "online": online,
+            "offline": offline,
+            "overwrite": allow_overwrite,
+            "use_tecton_managed_retries": tecton_managed_retries,
         }
-        return self._make_request(self.get_conn(), f"{JOBS_API_BASE}/{SUBMIT_JOB_METHOD}", data, verbose=True)
+        return self._make_request(
+            self.get_conn(), f"{JOBS_API_BASE}/{SUBMIT_JOB_METHOD}", data, verbose=True
+        )
 
-    def list_materialization_jobs(
-            self,
-            workspace: str,
-            feature_view: str
-    ) -> Dict:
+    def list_materialization_jobs(self, workspace: str, feature_view: str) -> Dict:
         """
         Lists all materialization jobs for a Feature View.
         :param workspace: Workspace name
@@ -160,16 +199,15 @@ class TectonHook(BaseHook):
         :return: A dict which contains a list of jobs.
         """
         data = {
-            'workspace': workspace,
-            'feature_view': feature_view,
+            "workspace": workspace,
+            "feature_view": feature_view,
         }
-        return self._make_request(self.get_conn(), f"{JOBS_API_BASE}/{LIST_JOB_METHOD}", data)
+        return self._make_request(
+            self.get_conn(), f"{JOBS_API_BASE}/{LIST_JOB_METHOD}", data
+        )
 
     def get_materialization_job(
-            self,
-            workspace: str,
-            feature_view: str,
-            job_id: str
+        self, workspace: str, feature_view: str, job_id: str
     ) -> Dict:
         """
         Gets a job
@@ -178,18 +216,13 @@ class TectonHook(BaseHook):
         :param job_id: Job ID
         :return:
         """
-        data = {
-            'workspace': workspace,
-            'feature_view': feature_view,
-            'job_id': job_id
-        }
-        return self._make_request(self.get_conn(), f"{JOBS_API_BASE}/{GET_JOB_METHOD}", data)
+        data = {"workspace": workspace, "feature_view": feature_view, "job_id": job_id}
+        return self._make_request(
+            self.get_conn(), f"{JOBS_API_BASE}/{GET_JOB_METHOD}", data
+        )
 
     def cancel_materialization_job(
-            self,
-            workspace: str,
-            feature_view: str,
-            job_id: str
+        self, workspace: str, feature_view: str, job_id: str
     ) -> Dict:
         """
         Cancels a running materialization job
@@ -199,18 +232,16 @@ class TectonHook(BaseHook):
         :param job_id:  Job ID
         :return:
         """
-        data = {
-            'workspace': workspace,
-            'feature_view': feature_view,
-            "job_id": job_id
-        }
-        return self._make_request(self.get_conn(), f"{JOBS_API_BASE}/{CANCEL_JOB_METHOD}", data, verbose=True)
+        data = {"workspace": workspace, "feature_view": feature_view, "job_id": job_id}
+        return self._make_request(
+            self.get_conn(), f"{JOBS_API_BASE}/{CANCEL_JOB_METHOD}", data, verbose=True
+        )
 
     def get_latest_ready_time(
-            self,
-            workspace: str,
-            feature_view: Optional[str] = None,
-            feature_service: Optional[str] = None
+        self,
+        workspace: str,
+        feature_view: Optional[str] = None,
+        feature_service: Optional[str] = None,
     ) -> Dict:
         """
 
@@ -219,17 +250,21 @@ class TectonHook(BaseHook):
         :param feature_service: [optional] feature service name: mutually exclusive with feature_view
         :return:
         """
-        assert (feature_service is None) != (feature_view is None), "Exactly one of feature_view or feature_service should be set"
+        assert (feature_service is None) != (
+            feature_view is None
+        ), "Exactly one of feature_view or feature_service should be set"
         data = {
-            'workspace': workspace,
+            "workspace": workspace,
         }
         if feature_view:
-            data['feature_view'] = feature_view
+            data["feature_view"] = feature_view
         if feature_service:
-            data['feature_service'] = feature_service
-        result = self._make_request(self.get_conn(), f"{JOBS_API_BASE}/{READINESS_METHOD}", data)
-        online_key = 'online_latest_ready_time'
-        offline_key = 'offline_latest_ready_time'
+            data["feature_service"] = feature_service
+        result = self._make_request(
+            self.get_conn(), f"{JOBS_API_BASE}/{READINESS_METHOD}", data
+        )
+        online_key = "online_latest_ready_time"
+        offline_key = "offline_latest_ready_time"
         if online_key not in result:
             result[online_key] = None
         elif result[online_key]:
@@ -245,4 +280,3 @@ class TectonHook(BaseHook):
     @classmethod
     def create(cls, conn_id: str):
         return cls(conn_id)
-
